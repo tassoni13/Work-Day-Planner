12b2933a22a9f52e812586a60b2d9cbdb9d81b70
Emily Tassoni <tassoni13@gmail.com>
2021-04-28 14:13:56 -0400

    Updated time block colors to reflect past, present, and future

--- script.js
+++ script.js
@@ -1,3 +1,14 @@
+var hour9 = $("#9")
+var hour10 = $("#10")
+var hour11 = $("#11")
+var hour12 = $("#12")
+var hour1 = $("#13")
+var hour2 = $("#14")
+var hour3 = $("#15")
+var hour4 = $("#15")
+var hour5 = $("#17")
+var time = moment();
+
 //Displays time at top of calendar
 var currentDay = document.querySelector("#currentDay")
 
@@ -5,3 +16,22 @@ currentDay.textContent = moment().format("dddd, MMMM Do YYYY");
 
 //Color codes each block based on if it is in the past, present, or future
 
+function blockColor() {
+    var hour = time.hours();
+    $(".time-block").each(function (){
+        var thisHour = parseInt($(this).attr("id"));
+
+        if (thisHour > hour) {
+            $(this).addClass("future")
+        }
+        else if (thisHour === hour) {
+            $(this).addClass("present");
+        }
+
+        else {
+            $(this).addClass("past");
+        }
+    })
+}
+
+blockColor();
